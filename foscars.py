2d56f4435ed16e1322f8824a22be2df4447911b4
ALVasir <16958247+ALVasir@users.noreply.github.com>
2025-03-24 08:04:53 +0100

    Update foscars.py
    sigue fallando

--- foscars.py
+++ foscars.py
@@ -16,41 +16,13 @@ def opcion1(cabecera,diccionario):
 
                 print(f"Año {anyo['año']}: {pelicula} - {origen}")
 
-# def opcion1(cabecera,diccionario):
-# # Listado de películas ganadoras de USA y extrajeras, ordenadas por años
-#     os.system('cls')
-#     print(cabecera)
-#     for anio in diccionario["Oscars"]:
-#         for categoria, detalle in anio["categorias"].items():
-#             if categoria.lower() == "mejor película":  # Filtrar solo ganadores de "Mejor Película"
-#                 # Extraer nombre de la película y nacionalidad
-#                 pelicula = detalle["titulo"] if isinstance(detalle, dict) else detalle
-#                 nacionalidad = detalle.get("nacionalidad", "Desconocida") if isinstance(detalle, dict) else "Desconocida"
-                
-#                 # Determinar si es de EE.UU. o extranjera
-#                 origen = "EE.UU." if "EE.UU." in nacionalidad else "Extranjera"
 
-#                 print(f"Año {anio['año']}: {pelicula} - {origen}")
 
 
 
 def opcion3():
     import json
  
-    # Opening JSON file
-    f = open('/media/usuario/SanDisk/A S I R/LENGUAJES/JSON/proyecto/oscars.json','r')
-    
-    # returns JSON object as 
-    # a dictionary
-    data = json.load(f)
-    
-    # Iterating through the json
-    # list
-    for i in data:
-        print(i)
-    
-    # Closing file
-    f.close()
 
 def opcion4(diccionario):
    for año in diccionario["Oscars"]["años"]:
